36512d207e199ef7ec495506356cb1916e073073
lmobarki <143443056+lmobarki@users.noreply.github.com>
2025-03-06 23:05:19 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,3 +3,10 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.querySelector('button[type="submit"]');
+  
+  submitButton.onmouseover = () => submitButton.style.backgroundColor = "#FFD700";
+  submitButton.onmouseout = () => submitButton.style.backgroundColor = "#FFFFFF";
+});
